305807be1ccae9e7b9753bc86a2501afacd9a869
sunssoldier <sunssoldier@gmail.com>
2020-06-24 13:45:26 -0400

    add env helper

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # spark-apache-delta
+
+This project relies on the python from within your venv, in the base directory of the repo (not included with repo, use whatever python version you like)
---
+++ set_env.sh
@@ -0,0 +1,10 @@
+conda deactivate #deactivate any active conda environments
+deactivate # deactivate any python venvs
+
+export SPARK_HOME=/usr/local/spark-3.0.0-bin-hadoop2.7/
+export PATH=$SPARK_HOME/bin:$PATH
+
+export PYSPARK_PYTHON=./venv/bin/python3.8
+
+#export PYSPARK_DRIVER_PYTHON=jupyter
+#export PYSPARK_DRIVER_PYTHON_OPTS='notebook'
